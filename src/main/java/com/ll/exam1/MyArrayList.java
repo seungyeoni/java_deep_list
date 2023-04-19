597226e9ec4c204c2caca96c3165ea3ab4ef7dba
seungyeoni <dnri33@naver.com>
2023-04-19 15:08:08 +0900

    add()

--- src/main/java/com/ll/exam1/MyArrayList.java
+++ src/main/java/com/ll/exam1/MyArrayList.java
@@ -1,7 +1,14 @@
 package com.ll.exam1;
 
 public class MyArrayList<T> {
+    private  int size = 0;
+
     public int size(){
-        return 0;
+        return size;
+    }
+
+    public boolean add(T element) {
+        size++;
+        return true;
     }
 }
--- src/test/java/com/ll/exam1/MyArrayListTest.java
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -3,13 +3,25 @@ package com.ll.exam1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyArrayListTest {
 
     @Test
-    void testSize(){
+    @DisplayName("size()")
+    void t1(){
         MyArrayList<String> list = new MyArrayList<>();
-        assertEquals(0, list.size());
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("add(\"사과\")")
+    void t2(){
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+        assertThat(list.size()).isEqualTo(2);
     }
 }
