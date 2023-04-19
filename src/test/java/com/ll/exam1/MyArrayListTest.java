0fdce0e4a37a68cc76cc2c744e4c82672479d3a7
seungyeoni <dnri33@naver.com>
2023-04-19 15:04:19 +0900

    size()

---
+++ src/test/java/com/ll/exam1/MyArrayListTest.java
@@ -0,0 +1,15 @@
+package com.ll.exam1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyArrayListTest {
+
+    @Test
+    void testSize(){
+        MyArrayList<String> list = new MyArrayList<>();
+        assertEquals(0, list.size());
+    }
+}
